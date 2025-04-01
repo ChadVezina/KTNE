221cc6bee1452765037ba3b793eb0004cec87328
cotelan <90422683+cotelan@users.noreply.github.com>
2025-04-01 16:23:26 -0400

    Refactorisation de la méthode get_solution pour simplifier la logique de détermination de la solution. Mise à jour de la méthode update_solution pour toujours définir le texte de la solution, même si aucune solution n'est trouvée.

--- src/module/_8/model/case.py
+++ src/module/_8/model/case.py
@@ -11,51 +11,15 @@ class Case:
             TypeFil.ETOILE: False,
             TypeFil.LUMIERE_ALLUMEE: False,
         }
-        self.type_solution: TypeSolution | None = None
 
     def placer_case(self, parent):
         self.label = Texte(parent, self.numero, 0, f"Fil {self.numero + 1}")
         self.solution = Texte(parent, self.numero, 1)
+        self.update_solution()
         self.bouton_rouge = BoutonCase(parent, self.numero, 2, "rouge", lambda: self.clic(TypeFil.ROUGE))
         self.bouton_bleu = BoutonCase(parent, self.numero, 3, "bleu", lambda: self.clic(TypeFil.BLEU))
         self.bouton_etoile = BoutonCase(parent, self.numero, 4, "★", lambda: self.clic(TypeFil.ETOILE))
         self.bouton_lumiere_allumee = BoutonCase(parent, self.numero, 5, "lumière allumée", lambda: self.clic(TypeFil.LUMIERE_ALLUMEE))
-
-    def get_solution0(self):
-        if self.types[TypeFil.ROUGE]:
-            if self.types[TypeFil.LUMIERE_ALLUMEE]:
-                if self.types[TypeFil.BLEU]:
-                    if self.types[TypeFil.ETOILE]:
-                        return TypeSolution.N
-                    else:
-                        return TypeSolution.S
-                else:
-                    return TypeSolution.B
-            else:
-                if self.types[TypeFil.ETOILE]:
-                    if self.types[TypeFil.BLEU]:
-                        return TypeSolution.P
-                    else:
-                        return TypeSolution.C
-                else:
-                    return TypeSolution.S
-        else:
-            if self.types[TypeFil.LUMIERE_ALLUMEE]:
-                if self.types[TypeFil.BLEU]:
-                    return TypeSolution.P
-                else:
-                    if self.types[TypeFil.ETOILE]:
-                        return TypeSolution.B
-                    else:
-                        return TypeSolution.N
-            else:
-                if self.types[TypeFil.BLEU]:
-                    if self.types[TypeFil.ETOILE]:
-                        return TypeSolution.N
-                    else:
-                        return TypeSolution.S
-                else:
-                    return TypeSolution.C
 
     def get_solution(self):
         if self.types[TypeFil.ROUGE]:
@@ -91,10 +55,7 @@ class Case:
 
     def update_solution(self):
         self.type_solution = self.get_solution()
-        if self.type_solution:
-            self.solution.set_texte(self.type_solution.str_value)
-        else:
-            self.solution.clear_texte()
+        self.solution.set_texte(self.type_solution.str_value)
 
     def clic(self, type: TypeFil):
         if self.types[type]:
