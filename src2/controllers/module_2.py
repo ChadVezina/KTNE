ed591707b6c63058fd29dd06aee4b2ce73b9fd6f
cotelan <90422683+cotelan@users.noreply.github.com>
2025-04-13 18:56:36 -0400

    Ajout de l'initialisation des questions dans les méthodes init des contrôleurs Module1 et Module2 pour améliorer la configuration de l'interface utilisateur lors des changements d'état d'authentification.

--- src2/controllers/module_2.py
+++ src2/controllers/module_2.py
@@ -8,12 +8,12 @@ class Module2Controller:
         self.view = view
         self.frame = self.view.frames["module2"]
         self.init()
+        self.frame.init_questions()
         self._bind()
 
     def init(self) -> None:
         self.frame.is_state = self.model.auth.is_state
         self.frame.update_state = self.model.auth.update
-        self.frame.init_questions()
 
     def _bind(self) -> None:
         self.frame.module1.config(command=self.module1)
