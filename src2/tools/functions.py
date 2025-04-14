e4b29fc8186bba269f245a849118cac374e5d521
cotelan <90422683+cotelan@users.noreply.github.com>
2025-04-13 22:37:09 -0400

    Refactor la fonction make_menu pour séparer les menus "Fichier" et "Instructions". Amélioration de la lisibilité et de la gestion des commandes.

--- src2/tools/functions.py
+++ src2/tools/functions.py
@@ -12,20 +12,23 @@ def make_menu(root: Tk, nouvelle: Callable, quitter: Callable):
     root.bind("<Control-n>", lambda event: nouvelle())
     root.bind("<Alt-F4>", lambda event: quitter())
 
-    popup_menu = Menu(root, tearoff = 0)
-    popup_menu.add_command(label = "Nouvelle", command = nouvelle, accelerator = "Ctrl+N")
-    popup_menu.add_separator()
-    popup_menu.add_command(label = "Quitter", command = quitter, accelerator = "Alt+F4")
+    popup_menu_fichier = Menu(root, tearoff = 0)
+    popup_menu_fichier.add_command(label = "Nouvelle", command = nouvelle, accelerator = "Ctrl+N")
+    popup_menu_fichier.add_separator()
+    popup_menu_fichier.add_command(label = "Quitter", command = quitter, accelerator = "Alt+F4")
 
-    menu = Menu(root)
-    menu.add_cascade(label = "Fichier", menu = popup_menu)
+    popup_menu_instructions = Menu(root, tearoff = 0)
     for search_index in range(len(Titre)):
         titre = Titre.get_value_by_index(search_index)
         new_titre = NewTitre.get_value_by_index(search_index)
         contenu = Contenu.get_value_by_index(search_index)
         if titre is None or new_titre is None or contenu is None:
             continue
-        menu.add_command(label = f"Instructions: {titre}", command = lambda root = root, titre = new_titre, contenu = contenu: show_instructions(root, titre, contenu))
+        popup_menu_instructions.add_command(label = titre, command = lambda root = root, titre = new_titre, contenu = contenu: show_instructions(root, titre, contenu))
+
+    menu = Menu(root)
+    menu.add_cascade(label = "Fichier", menu = popup_menu_fichier)
+    menu.add_cascade(label = "Instructions", menu = popup_menu_instructions)
 
     root.config(menu = menu)
 
